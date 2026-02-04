d29d025a08c4aaa5a7466d3219d71c2692e6e480
Horoshko Vitaliy <horoshko29@gmail.com>
2026-02-04 19:10:16 +0200

    task solution #1

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,63 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.scss';
 import { GoodsList } from './GoodsList';
 
-// import { getAll, get5First, getRed } from './api/goods';
+import { getAll, get5First, getRedGoods } from './api/goods';
+import { Good } from './types/Good';
 // or
 // import * as goodsAPI from './api/goods';
 
-export const App: React.FC = () => (
-  <div className="App">
-    <h1>Dynamic list of Goods</h1>
+export const App: React.FC = () => {
+  const [goods, setGoods] = useState<Good[]>([]);
+  const [errorMessage, setErrorMessage] = useState('');
 
-    <button type="button" data-cy="all-button">
-      Load all goods
-    </button>
+  function handleLoadAllGoods() {
+    setErrorMessage('');
 
-    <button type="button" data-cy="first-five-button">
-      Load 5 first goods
-    </button>
+    getAll()
+      .then(setGoods)
+      .catch(() => setErrorMessage('Oops, something went wrong!'));
+  }
 
-    <button type="button" data-cy="red-button">
-      Load red goods
-    </button>
+  function handleFiveFirst() {
+    setErrorMessage('');
 
-    <GoodsList goods={[]} />
-  </div>
-);
+    get5First()
+      .then(setGoods)
+      .catch(() => setErrorMessage('Oops, something went wrong!'));
+  }
+
+  function handleRedGoods() {
+    setErrorMessage('');
+
+    getRedGoods()
+      .then(setGoods)
+      .catch(() => setErrorMessage('Oops, something went wrong!'));
+  }
+
+  return (
+    <div className="App">
+      <h1>Dynamic list of Goods</h1>
+
+      <button type="button" data-cy="all-button" onClick={handleLoadAllGoods}>
+        Load all goods
+      </button>
+
+      <button
+        type="button"
+        data-cy="first-five-button"
+        onClick={handleFiveFirst}
+      >
+        Load 5 first goods
+      </button>
+
+      <button type="button" data-cy="red-button" onClick={handleRedGoods}>
+        Load red goods
+      </button>
+
+      {errorMessage && <p className="Error">{errorMessage}</p>}
+
+      {!errorMessage && goods.length > 0 && <GoodsList goods={goods} />}
+    </div>
+  );
+};
